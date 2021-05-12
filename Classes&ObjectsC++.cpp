e4a2090f7e4a80610c3349041c6615c4227b4a86
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-05-12 08:22:20 +0530

    Classes and Objects in C++

---
+++ Classes&ObjectsC++.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<cmath>
+#include<unordered_map>
+#include<unordered_set>
+#include<map>
+#include<bitset>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<set>
+#include<chrono>
+#include<list>
+#include<climits>
+#include<iomanip>
+
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long 
+#define mod 1000000007
+
+
+class School{
+public:
+  string name;
+  string place;
+
+  void display(){
+    cout<<name<<"\n"<<place;
+  }
+};
+
+
+int main(void)
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);cout.tie(NULL);
+
+  //code goes down here
+	School s;
+  s.name = "Bal Bharti Public School";
+  s.place = "Bherwa";
+
+  s.display();
+
+	
+}
